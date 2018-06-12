3960063e8a4b1895efbe4a35ae0a363c98154f1f
CJ <cj@JosephdeMacBook-Pro.local>
2018-06-13 02:15:19 +0800

    Complete v1.1

--- startup-script.sh
+++ startup-script.sh
@@ -9,5 +9,10 @@ usermod -aG mail demo
 sed -i '$adisable_plaintext_auth = no' /etc/dovecot/conf.d/10-auth.conf
 sed -i '$amail_location = mbox:~/mail:INBOX=/var/mail/%u' /etc/dovecot/conf.d/10-mail.conf
 systemctl restart dovecot
-echo hello1 | mail -s "test1" demo@localhost.
+# Prepare some data for imap/pop3 test
+for i in {1..5}
+do
+    #echo "Welcome $i times"
+    echo hello$i | mail -s "test$i" demo@localhost.
+done
 
