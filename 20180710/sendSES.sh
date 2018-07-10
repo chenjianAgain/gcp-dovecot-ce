70d9fa466550f4793d2700f2a1025c16d4b3399b
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-10 18:38:57 +0800

    Update sendSES.sh

--- 20180710/sendSES.sh
+++ 20180710/sendSES.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 current=`date "+%Y-%m-%d %H:%M:%S"` 
 aws ses send-email \
---from "chenjian.aws@gmail.com" \
---destination "ToAddresses=demo@josephaws.one" \  
---message "Subject={Data=$current,Charset=utf8},Body={Text={Data=ses says hi,Charset=utf8},Html={Data=,Charset=utf8}}"
+  --from "john@gmail.com" \
+  --destination "ToAddresses=mike@gmail.com" \
+  --message "Subject={Data=$current,Charset=utf8},Body={Text={Data=ses says hi,Charset=utf8},Html={Data=,Charset=utf8}}"
