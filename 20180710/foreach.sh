7bf527e6e890c43bf9bed23d484711d449a38af9
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-10 18:25:34 +0800

    Create foreach.sh

---
+++ 20180710/foreach.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+          
+for i in {1..3}
+do
+    #echo "Welcome $i times"
+    current=`date "+%Y-%m-%d %H:%M:%S"` 
+    # echo "hello-$i at $current" | mail -s "test$i-$current" demo@localhost.
+done
