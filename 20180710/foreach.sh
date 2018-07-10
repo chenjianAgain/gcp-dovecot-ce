13c14e4f2a7e545013c0f0008c73486edd63b8bb
Joseph <joseph.siyi@gmail.com>
2018-07-10 11:14:44 +0000

    add for

--- 20180710/foreach.sh
+++ 20180710/foreach.sh
@@ -5,5 +5,5 @@ do
     echo "Welcome $i times"
     current=`date "+%Y-%m-%d %H:%M:%S"` 
     # echo "hello-$i at $current" | mail -s "test$i-$current" demo@localhost.
-    ./sendSES.sh $i
+    ./sendSES.sh $current
 done
