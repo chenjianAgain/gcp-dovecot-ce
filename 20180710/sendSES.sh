f39455b71198bed346b945ea1105c432c6023004
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-10 18:33:24 +0800

    Update sendSES.sh

--- 20180710/sendSES.sh
+++ 20180710/sendSES.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-aws ses send-email   --from "chenjian.aws@gmail.com"   --destination "ToAddresses=demo@josephaws.one"   --message "Subject={Data=from ses,Charset=utf8},Body={Text={Data=ses says hi,Charset=utf8},Html={Data=,Charset=utf8}}"
+aws ses send-email \
+--from "chenjian.aws@gmail.com" \
+--destination "ToAddresses=demo@josephaws.one" \  
+--message "Subject={Data=from ses,Charset=utf8},Body={Text={Data=ses says hi,Charset=utf8},Html={Data=$0,Charset=utf8}}"
