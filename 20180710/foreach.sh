654c15bd6a6daf3a0b673cde3a323046a8675a72
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-11 14:27:39 +0800

    Update foreach.sh

--- 20180710/foreach.sh
+++ 20180710/foreach.sh
@@ -2,7 +2,7 @@
           
 for i in {1..3}
 do
-    echo "Welcome $i times"
+    echo "Sent $i email from SES"
     current=`date "+%Y-%m-%d %H:%M:%S"` 
     # echo "hello-$i at $current" | mail -s "test$i-$current" demo@localhost.
     ./sendSES.sh "$current"
