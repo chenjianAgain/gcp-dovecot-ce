016056c4557f5fc4c6e0adafa363c9dd6e8181a5
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-11 14:23:23 +0800

    Update sendSES.sh

--- 20180710/sendSES.sh
+++ 20180710/sendSES.sh
@@ -3,4 +3,4 @@ current=$(date -d "+ 8 hour" +'%Y-%m-%d %H:%M:%S')
 aws ses send-email \
   --from "chenjian.aws@gmail.com" \
   --destination "ToAddresses=demo@josephaws.one" \
-  --message "Subject={Data=$current,Charset=utf8},Body={Text={Data=$current ses $1,Charset=utf8},Html={Data=,Charset=utf8}}"
+  --message "Subject={Data=$current,Charset=utf8},Body={Text={Data=$current sent from aws SES $1,Charset=utf8},Html={Data=,Charset=utf8}}"
