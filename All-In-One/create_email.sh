499279d1af456afeafead155f162ca96f6d45af4
Joseph Chenkie <cj.efuture@gmail.com>
2018-07-25 14:28:36 +0800

    Update create_email.sh

--- All-In-One/create_email.sh
+++ All-In-One/create_email.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+for i in {1..3}
+do
+    #echo "Welcome $i times"
+    current=`date "+%Y-%m-%d %H:%M:%S"` 
+    echo "hello-$i at $current" | mail -s "test$i-$current" demo@localhost.
+done
